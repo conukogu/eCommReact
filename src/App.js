546de4c5c8608f236ac5d77ddc89510e387f3ffb
Chioma Onukogu <conukogu1@gmail.com>
2022-03-21 15:37:50 -0400

    filter

--- src/App.js
+++ src/App.js
@@ -1,95 +1,74 @@
 import React, {useState} from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react'
-import SwiperCore, {Navigation, Pagination, Controller, Thumbs, Autoplay} from 'swiper'
-import  'swiper/css/bundle';
+import Filter from './Components/Filter';
 import './styles.css';
 
-SwiperCore.use([Navigation, Pagination, Controller,  Thumbs]);
+
+// const fs = require('casual');
+// const {
+//     application
+// } = require('express');
+// const express = require('express');
+// const {
+//     format
+// } = require('path');
+// const port = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.use(function (req, res, next) {
+//     // console.log("Middleware called");
+//     next();
+//   });
+
+// //API Middleware
+// app.use(express.json()); //accepts data in JSON format
+
+// app.use(express.urlencoded({
+//     extended: false
+// })); //reads the form
+
+// // app.use(express.static("data")); //serves public folder as a static folder
+
+
+
+// //API Routes
+// var data = fs.readFileSync('testy.json', 'utf8', function (err, data) {});
+
+
+
+// app.get('/', (req, res) => {
+//     res.send(data);
+
+//     try {
+//       data = JSON.parse(data)
+//       console.log(data)
+//   } catch (e) {
+//       return data
+//   }
+// })
+
+// app.get('/filter', (req, res) => {
+//     const arr3 = data.filter(function(item){
+//     return item.name == "Steven Gaston";         
+// })
+// res.send(arr3);
+// })
+
+// app.listen(8000, () => {
+//     console.log(`console server listening on port 4000`)
+// })
+
 
 
 function App() {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  const [controlledSwiper, setControlledSwiper] = useState(null);
-  
-  const slides =[];
-
-  for (let i = 0; i < 5; i += 1){
-    slides.push(
-      <SwiperSlide  key={`slide-${i}`} tag= "li">
-        <img src={`https://picsum.photos/id/${i+1}/500/300`} alt = {`Slide ${i}`}/>
-      </SwiperSlide>
-    );
-  }
-
-  const thumbs =[];
-
-  for (let i = 0; i < 5; i += 1){
-    thumbs.push(
-      <SwiperSlide  key={`slide-${i}`} tag= "li">
-        <img src={`https://picsum.photos/id/${i}/163/100`} alt = {`Thumbnail ${i}`}/>
-      </SwiperSlide>
-    );
-  }
-
-  const slides2 =[];
-  
-  for (let i = 5; i < 10; i += 1){
-    slides2.push(
-      <SwiperSlide  key={`slide-${i}`} tag= "li">
-        <img src={`https://picsum.photos/id/${i+1}/500/300`} alt = {`Slide ${i}`}/>
-      </SwiperSlide>
-    );
-  }
-
-  const slides3 =[]
     
-  for (let i = 0; i < 5; i += 1){
-    slides3.push(
-      <SwiperSlide  key={`slide-${i}`} tag= "li">
-        <img src={`https://source.unsplash.com/random/800x800/?img=1`} alt = {`Thumbnail ${i}`} 
-        autoHeight={false}
-        className='hope'
-       />
-      </SwiperSlide>
-    );
-  }
-  
-
-
+ 
   return (
-    <React.Fragment>
-      <Swiper  
-      id= "main" 
-      // thumbs={{swiper: thumbsSwiper}}
-      // controller = {{control: controlledSwiper}}
-      tag= "section" 
-      wrapperTag='ul' 
-      navigation 
-      pagination
-      height={1} 
-      spaceBetween={0}
-      slidesPerView='auto'
-      autoHeight={false}
-      onInit={(swiper) => console.log(`Swiper initialized`, swiper)}
-      onSlideChange={(swiper) => console.log(`Slide index changed to:`, swiper.activeIndex)}
-      onReachEnd={()=> console.log('Swiper end reached')
-      }
-      onReachBeginning={()=> console.log('Swiper start reached')}>
-        {slides3}
-      </Swiper>
-
-      {/* <Swiper 
-      id='thumbs'
-      spaceBetween={5}
-      slidesPerView={3} 
-      onSwiper={setThumbsSwiper}>
-        {thumbs}
-      </Swiper> */}
-
-      {/* <Swiper id="controller" onSwiper={setControlledSwiper}>
-        {slides2}
-      </Swiper> */}
-    </React.Fragment>
+    <>
+ <div>
+   <Filter/>
+ </div>
+    </>
   );
 }
 
